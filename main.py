571dcc7a4ab26eff698dd572f3f1f3b66d13afdc
Saksham Mahajan <mahajansaksham38@gmail.com>
2017-07-05 22:40:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ def get_user_id(insta_username):
         if user_info['meta']['code'] == 200:
             if len(user_info['data']):
                 return user_info['data'][0]['id']
-                print user_info['data'][0]['id']
             else:
                 return None
         else:
@@ -94,6 +93,51 @@ def get_user_post(insta_username):
     else:
         print 'Status code other than 200 received!'
 
+def get_post_id(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_media = requests.get(request_url).json()
+
+    if user_media['meta']['code'] == 200:
+        if len(user_media['data']):
+            return user_media['data'][0]['id']
+        else:
+            print 'There is no recent post of the user!'
+            exit()
+    else:
+        print 'Status code other than 200 received!'
+        exit()
+
+def like_a_post(insta_username):
+    media_id=get_post_id(insta_username)
+    request_url=(BASE_URL+"media/%s/likes") %(media_id)
+    payload={"access_token":APP_ACCESS_TOKEN}
+    print 'POST request url : %s' % (request_url)
+    post_a_like = requests.post(request_url, payload).json()
+    if post_a_like['meta']['code'] == 200:
+        print 'Like was successful!'
+    else:
+        print 'Your like was unsuccessful. Try again!'
+
+def post_a_comment(insta_username):
+    media_id = get_post_id(insta_username)
+    comment_text = raw_input("Your comment: ")
+    payload = {"access_token": APP_ACCESS_TOKEN, "text" : comment_text}
+    request_url = (BASE_URL + 'media/%s/comments') % (media_id)
+    print 'POST request url : %s' % (request_url)
+
+    make_comment = requests.post(request_url, payload).json()
+
+    if make_comment['meta']['code'] == 200:
+        print "Successfully added a new comment!"
+    else:
+        print "Unable to add comment. Try again!"
+
+
 def start_bot():
         while True:
             print '\n'
@@ -104,9 +148,9 @@ def start_bot():
             print "c.Get your own recent post\n"
             print "d.Get the recent post of a user by username\n"
             # print "e.Get a list of people who have liked the recent post of a user\n"
-            # print "f.Like the recent post of a user\n"
+            print "f.Like the recent post of a user\n"
             # print "g.Get a list of comments on the recent post of a user\n"
-            # print "h.Make a comment on the recent post of a user\n"
+            print "h.Make a comment on the recent post of a user\n"
             # print "i.Delete negative comments from the recent post of a user\n"
             print "j.Exit"
 
@@ -124,15 +168,15 @@ def start_bot():
             # elif choice=="e":
             #    insta_username = raw_input("Enter the username of the user: ")
             #    get_like_list(insta_username)
-            # elif choice=="f":
-            #    insta_username = raw_input("Enter the username of the user: ")
-            #    like_a_post(insta_username)
+            elif choice=="f":
+                insta_username = raw_input("Enter the username of the user: ")
+                like_a_post(insta_username)
             # elif choice=="g":
             #    insta_username = raw_input("Enter the username of the user: ")
             #    get_comment_list(insta_username)
-            # elif choice=="h":
-            #    insta_username = raw_input("Enter the username of the user: ")
-            #    make_a_comment(insta_username)
+            elif choice=="h":
+                insta_username = raw_input("Enter the username of the user: ")
+                post_a_comment(insta_username)
             # elif choice=="i":
             #    insta_username = raw_input("Enter the username of the user: ")
             #    delete_negative_comment(insta_username)
